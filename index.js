7da1a9af1b0872eb48b243a084d2e82c1efbc9b3
AlucardSanin <alucardsanin@gmail.com>
2023-01-20 11:10:24 -0400

    Fixed not returning from h1

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ input.addEventListener('keydown', (e) => {
 
   if (e.key === 'Backspace' && text.value === '') {
     input.placeholder = 'Type /1 and hit enter to start typing an H1 Text';
+    type = 'p';
   } else if (e.key === 'Enter' && text.value.trim().length > 0) {
     textwriter(type);
     type = 'p';
